04f04f45064076af218c3d219eb459607ea5dade
olugbenga adetayo <olugbengaadetayo@gmail.com>
2022-11-16 16:07:01 -0800

    all tasks

--- errors1.c
+++ errors1.c
@@ -18,10 +18,10 @@ int _erratoi(char *s)
 	{
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			result *= 10
+			result *= 10;
 			result += (s[i] - '0');
 			if (result > INT_MAX)
-				return (-1)
+				return (-1);
 		}
 		else
 			return (-1);
--- tokenizer.c
+++ tokenizer.c
@@ -76,7 +76,7 @@ char **strtow2(char *str, char d)
 			i++;
 		k = 0;
 		while (str[i + k] != d && str[i + k] && str[i + k] != d)
-			K++;
+			k++;
 		s[j] = malloc((k + 1) * sizeof(char));
 		if (!s[j])
 		{
